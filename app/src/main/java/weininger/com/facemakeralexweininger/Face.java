6cc7e3805667ab171c105180ffac8451639c5349
Alex <alex.weininger@live.com>
2018-09-21 22:17:05 -0700

    implemented Face class with empty methods

--- app/src/main/java/weininger/com/facemakeralexweininger/Face.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/Face.java
@@ -1,4 +1,31 @@
 package weininger.com.facemakeralexweininger;
 
-public class Face {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.SurfaceView;
+
+public class Face extends SurfaceView {
+
+    int skinColor;
+    int eyeColor;
+    int hairColor;
+    int hairStyle;
+
+    public Face(Context context) {
+        super(context);
+        randomize();
+    }
+
+    public Face(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        randomize();
+    }
+
+    protected void randomize() {
+
+    }
+
+    protected void OnDraw() {
+
+    }
 }
