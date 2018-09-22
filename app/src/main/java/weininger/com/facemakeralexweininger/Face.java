509d8cce766c967e9431d1220438c1ac1350213f
Alex <alex.weininger@live.com>
2018-09-22 16:45:18 -0700

    formatting and header comments

--- app/src/main/java/weininger/com/facemakeralexweininger/Face.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/Face.java
@@ -1,3 +1,10 @@
+/*
+ *  Face.java - class contains methods to draw the face
+ *
+ *  @author: Alex Weininger
+ */
+
+
 package weininger.com.facemakeralexweininger;
 
 import android.content.Context;
@@ -12,147 +19,146 @@ import java.util.Random;
 
 public class Face extends SurfaceView {
 
-    int skinColor;
-    int eyeColor;
-    int hairColor;
-    int hairStyle;
+	int skinColor;
+	int eyeColor;
+	int hairColor;
+	int hairStyle;
 
-    int color;
+	int color;
 
-    int selectedItem;
+	int selectedItem;
 
-    public Face(Context context) {
-        super(context);
-        setWillNotDraw(false);
-        randomize();
-    }
+	public Face(Context context) {
+		super(context);
+		setWillNotDraw(false);
+		randomize();
+	}
 
-    public Face(Context context, AttributeSet attrs) {
-        super(context, attrs);
-        setWillNotDraw(false);
-        randomize();
-    }
+	public Face(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		setWillNotDraw(false);
+		randomize();
+	}
 
-    protected void randomize() {
-        Random random = new Random();
+	protected void randomize() {
+		Random random = new Random();
 
-        this.hairStyle = random.nextInt(3);
+		this.hairStyle = random.nextInt(3);
 
-        this.skinColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-        this.eyeColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
-        this.hairColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+		this.skinColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+		this.eyeColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
+		this.hairColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
 		invalidate();
-        Log.d("user", "face randomized");
-    }
+		Log.d("user", "face randomized");
+	}
 
 
-    public void onDraw(Canvas canvas) {
-    	canvas.drawColor(-1);
+	public void onDraw(Canvas canvas) {
+		canvas.drawColor(-1);
 
 		drawHairStyle(canvas);
 		drawSkin(canvas);
-        drawEyes(canvas);
-    }
+		drawEyes(canvas);
+	}
 
-    protected void drawHairStyle(Canvas canvas) {
-	    int x = canvas.getWidth();
-	    int y = canvas.getHeight();
-	    int width = y/4;
+	protected void drawHairStyle(Canvas canvas) {
+		int x = canvas.getWidth();
+		int y = canvas.getHeight();
+		int width = y / 4;
 
-	    Paint paint = new Paint();
-	    paint.setStyle(Paint.Style.FILL);
-	    paint.setColor(this.hairColor);
+		Paint paint = new Paint();
+		paint.setStyle(Paint.Style.FILL);
+		paint.setColor(this.hairColor);
 
-    	if(this.hairStyle == 1) {
-			canvas.drawCircle(x/2, y/2 - width/3, y/4, paint);
-	    } else if (this.hairStyle == 2) {
-		    canvas.drawRect(x/2 - 100, y/2 - width - 40, x/2 - 80, y/2 + y/4, paint);
-		    canvas.drawRect(x/2, y/2 - width - 50, x/2 + 20, y/2 + y/4, paint);
-		    canvas.drawRect(x/2 + 100, y/2 - width - 40, x/2 + 120, y/2 + y/4, paint);
-		    canvas.drawRect(x/2 - 140, y/2 - width - 40, x/2 - 120, y/2 + y/4, paint);
-		    canvas.drawRect(x/2 + 140, y/2 - width - 40, x/2 + 160, y/2 + y/4, paint);
+		if (this.hairStyle == 1) {
+			canvas.drawCircle(x / 2, y / 2 - width / 3, y / 4, paint);
+		} else if (this.hairStyle == 2) {
+			canvas.drawRect(x / 2 - 100, y / 2 - width - 40, x / 2 - 80, y / 2 + y / 4, paint);
+			canvas.drawRect(x / 2, y / 2 - width - 50, x / 2 + 20, y / 2 + y / 4, paint);
+			canvas.drawRect(x / 2 + 100, y / 2 - width - 40, x / 2 + 120, y / 2 + y / 4, paint);
+			canvas.drawRect(x / 2 - 140, y / 2 - width - 40, x / 2 - 120, y / 2 + y / 4, paint);
+			canvas.drawRect(x / 2 + 140, y / 2 - width - 40, x / 2 + 160, y / 2 + y / 4, paint);
 
-		    canvas.drawRect(x/2 - width, y/2 - width, x/2 + width, y/2 + y/4, paint);
-	    } else if (this.hairStyle == 3) {
-		    canvas.drawCircle(x/2 + 200, y/2 - width/3, y/6, paint);
-		    canvas.drawCircle(x/2, y/2 - width/3, y/6, paint);
-		    canvas.drawCircle(x/2 - 200, y/2 - width/3, y/6, paint);
-	    }
-    }
+			canvas.drawRect(x / 2 - width, y / 2 - width, x / 2 + width, y / 2 + y / 4, paint);
+		} else if (this.hairStyle == 3) {
+			canvas.drawCircle(x / 2 + 200, y / 2 - width / 3, y / 6, paint);
+			canvas.drawCircle(x / 2, y / 2 - width / 3, y / 6, paint);
+			canvas.drawCircle(x / 2 - 200, y / 2 - width / 3, y / 6, paint);
+		}
+	}
 
-    protected void drawSkin(Canvas canvas) {
-	    int x = canvas.getWidth();
-	    int y = canvas.getHeight();
-	    int radius = y/3;
+	protected void drawSkin(Canvas canvas) {
+		int x = canvas.getWidth();
+		int y = canvas.getHeight();
+		int radius = y / 3;
 
-	    Paint skinPaint = new Paint();
-	    skinPaint.setColor(this.skinColor);
-	    skinPaint.setStyle(Paint.Style.FILL);
+		Paint skinPaint = new Paint();
+		skinPaint.setColor(this.skinColor);
+		skinPaint.setStyle(Paint.Style.FILL);
 
-	    canvas.drawCircle(x/2, (y*2)/3, radius, skinPaint);
-    }
+		canvas.drawCircle(x / 2, (y * 2) / 3, radius, skinPaint);
+	}
 
-    protected void drawEyes(Canvas canvas) {
-	    int x = canvas.getWidth();
-    	int y = canvas.getHeight();
-    	int radius = 70;
+	protected void drawEyes(Canvas canvas) {
+		int x = canvas.getWidth();
+		int y = canvas.getHeight();
+		int radius = 70;
 
-    	Paint eyePaint = new Paint();
-    	eyePaint.setColor(this.eyeColor);
-    	eyePaint.setStyle(Paint.Style.FILL);
+		Paint eyePaint = new Paint();
+		eyePaint.setColor(this.eyeColor);
+		eyePaint.setStyle(Paint.Style.FILL);
 
-    	canvas.drawCircle(x/2 - radius*2, y/2, radius, eyePaint);
-	    canvas.drawCircle(x/2 + radius*2, y/2, radius, eyePaint);
+		canvas.drawCircle(x / 2 - radius * 2, y / 2, radius, eyePaint);
+		canvas.drawCircle(x / 2 + radius * 2, y / 2, radius, eyePaint);
 
-    }
+	}
 
 
-    protected void setHairStyle(String hairStyleString) {
-        switch (hairStyleString) {
-            case "Style 1":
-                this.hairStyle = 1;
+	protected void setHairStyle(String hairStyleString) {
+		switch (hairStyleString) {
+			case "Style 1":
+				this.hairStyle = 1;
 
-                break;
-            case "Style 2":
-                this.hairStyle = 2;
-                break;
-            case "Style 3":
-                this.hairStyle = 3;
-                break;
-            default:
-                this.hairStyle = 0;
-                Log.d("user", "did not find hairStyleString in switch statement - Face.setHairStyle: STRING: " + hairStyleString);
-        }
-        Log.d("user", "style set to: " + this.hairStyle);
-        invalidate();
-    }
+				break;
+			case "Style 2":
+				this.hairStyle = 2;
+				break;
+			case "Style 3":
+				this.hairStyle = 3;
+				break;
+			default:
+				this.hairStyle = 0;
+				Log.d("user", "did not find hairStyleString in switch statement - Face.setHairStyle: STRING: " + hairStyleString);
+		}
+		Log.d("user", "style set to: " + this.hairStyle);
+		invalidate();
+	}
 
-    public void setSelectedItem(int item) {
-    	this.selectedItem = item;
-    }
+	public void setSelectedItem(int item) {
+		this.selectedItem = item;
+	}
 
-    public int getSelectedItemColor() {
-    	if(this.selectedItem == 0) {
-    		return this.hairColor;
-	    } else if(this.selectedItem == 1) {
-    		return this.eyeColor;
-	    } else if(this.selectedItem == 2) {
-    		return this.skinColor;
-	    } else {
-    		return 0;
-	    }
-    }
+	public int getSelectedItemColor() {
+		if (this.selectedItem == 0) {
+			return this.hairColor;
+		} else if (this.selectedItem == 1) {
+			return this.eyeColor;
+		} else if (this.selectedItem == 2) {
+			return this.skinColor;
+		} else {
+			return 0;
+		}
+	}
 
-    public void setItemColor(int color) {
-	    if(this.selectedItem == 0) {
-		    this.hairColor = color;
-	    } else if(this.selectedItem == 1) {
-		    this.eyeColor = color;
-	    } else if(this.selectedItem == 2) {
-		    this.skinColor = color;
-	    }
-    }
-
+	public void setItemColor(int color) {
+		if (this.selectedItem == 0) {
+			this.hairColor = color;
+		} else if (this.selectedItem == 1) {
+			this.eyeColor = color;
+		} else if (this.selectedItem == 2) {
+			this.skinColor = color;
+		}
+	}
 
 
 }
--- app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
@@ -1,9 +1,15 @@
+/*
+ *  MainActivity.java - main class that handles logic of app
+ *
+ *  @author: Alex Weininger
+ */
+
+
 package weininger.com.facemakeralexweininger;
 
 import android.app.Activity;
 import android.graphics.Canvas;
 import android.graphics.Color;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
@@ -21,202 +27,173 @@ import static android.graphics.Color.red;
 
 public class MainActivity extends Activity {
 
-    Face face;
-    Canvas canvas;
-
-	RadioGroup radioButtonGroup;
-	RadioButton radioButtonHair;
-	RadioButton radioButtonEyes;
-	RadioButton radioButtonSkin;
-
+	Face face;
+	Canvas canvas;
 	Spinner styleSpinner;
-
 	SeekBar seekBar_r, seekBar_g, seekBar_b;
 
-	int selectedItem;
-
 	@Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-        canvas = new Canvas();
-        face = (Face) findViewById(R.id.face);
+		canvas = new Canvas();
+		face = findViewById(R.id.face);
 
-        seekBar_r = (SeekBar) findViewById(R.id.seekBar_r);
-        seekBar_g = (SeekBar) findViewById(R.id.seekBar_g);
-        seekBar_b = (SeekBar) findViewById(R.id.seekBar_b);
+		seekBar_r = findViewById(R.id.seekBar_r);
+		seekBar_g = findViewById(R.id.seekBar_g);
+		seekBar_b = findViewById(R.id.seekBar_b);
 
-        seekBar_r.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-	        @Override
-	        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+		seekBar_r.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
-		        int color = face.getSelectedItemColor();
-		        int blue = blue(color);
-		        int green = green(color);
+				int color = face.getSelectedItemColor();
+				int blue = blue(color);
+				int green = green(color);
 
-		        color = Color.argb(255, i, green, blue);
+				color = Color.argb(255, i, green, blue);
 
-		        if(b) {
-			        face.setItemColor(color);
-			        face.invalidate();
-		        }
+				if (b) {
+					face.setItemColor(color);
+					face.invalidate();
+				}
 
-	        }
+			}
 
-	        @Override
-	        public void onStartTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
 
-	        }
+			}
 
-	        @Override
-	        public void onStopTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
 
-	        }
-        });
+			}
+		});
 
-        seekBar_g.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-	        @Override
-	        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-		        int color = face.getSelectedItemColor();
-		        int blue = blue(color);
-		        int red = red(color);
+		seekBar_g.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+				int color = face.getSelectedItemColor();
+				int blue = blue(color);
+				int red = red(color);
 
-		        color = Color.argb(255, red, i, blue);
+				color = Color.argb(255, red, i, blue);
 
-		        if(b) {
-			        face.setItemColor(color);
-			        face.invalidate();
-		        }
+				if (b) {
+					face.setItemColor(color);
+					face.invalidate();
+				}
 
-	        }
+			}
 
-	        @Override
-	        public void onStartTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
 
-	        }
+			}
 
-	        @Override
-	        public void onStopTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
 
-	        }
-        });
+			}
+		});
 
-        seekBar_b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-	        @Override
-	        public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-		        int color = face.getSelectedItemColor();
-		        int red = red(color);
-		        int green = green(color);
+		seekBar_b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+			@Override
+			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
+				int color = face.getSelectedItemColor();
+				int red = red(color);
+				int green = green(color);
 
-		        color = Color.argb(255, red, green, i);
+				color = Color.argb(255, red, green, i);
 
-		        if(b) {
-			        face.setItemColor(color);
-			        face.invalidate();
-		        }
+				if (b) {
+					face.setItemColor(color);
+					face.invalidate();
+				}
 
 
-	        }
+			}
 
-	        @Override
-	        public void onStartTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStartTrackingTouch(SeekBar seekBar) {
 
-	        }
+			}
 
-	        @Override
-	        public void onStopTrackingTouch(SeekBar seekBar) {
+			@Override
+			public void onStopTrackingTouch(SeekBar seekBar) {
 
-	        }
-        });
+			}
+		});
 
-        Button randomButton = (Button) findViewById(R.id.button_random);
+		Button randomButton = findViewById(R.id.button_random);
 
-        final RadioGroup radioButtonGroup = (RadioGroup) findViewById(R.id.radioGroup_itemSelector);
-        final RadioButton radioButtonHair = (RadioButton) findViewById(R.id.radioButton_hair);
-        final RadioButton radioButtonEyes = (RadioButton) findViewById(R.id.radioButton_eyes);
-        final RadioButton radioButtonSkin = (RadioButton) findViewById(R.id.radioButton_skin);
+		final RadioGroup radioButtonGroup = findViewById(R.id.radioGroup_itemSelector);
+		final RadioButton radioButtonHair = findViewById(R.id.radioButton_hair);
+		final RadioButton radioButtonEyes = findViewById(R.id.radioButton_eyes);
+		final RadioButton radioButtonSkin = findViewById(R.id.radioButton_skin);
 
-        radioButtonGroup.check(R.id.radioButton_hair);
-        face.setSelectedItem(0);
+		radioButtonGroup.check(R.id.radioButton_hair);
+		face.setSelectedItem(0);
 
-        radioButtonGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
-            @Override
-            public void onCheckedChanged(RadioGroup radioGroup, int i) {
-                Log.d("user", "radio group onCheckChange listener value: " + i);
+		radioButtonGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+			@Override
+			public void onCheckedChanged(RadioGroup radioGroup, int i) {
+				Log.d("user", "radio group onCheckChange listener value: " + i);
 
-	            if(i == radioButtonHair.getId()) {
-		            face.setSelectedItem(0);
+				if (i == radioButtonHair.getId()) {
+					face.setSelectedItem(0);
 
-	            } else if (i == radioButtonEyes.getId()) {
-		            face.setSelectedItem(1);
-	            } else if (i == radioButtonSkin.getId()) {
-		            face.setSelectedItem(2);
-	            }
+				} else if (i == radioButtonEyes.getId()) {
+					face.setSelectedItem(1);
+				} else if (i == radioButtonSkin.getId()) {
+					face.setSelectedItem(2);
+				}
 
-	            setSeekBarValues(face.getSelectedItemColor());
+				setSeekBarValues(face.getSelectedItemColor());
 
-            }
-        });
+			}
+		});
 
-        randomButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Log.d("user", "random button click listener");
-                face.randomize();
-                setSeekBarValues(face.getSelectedItemColor());
-                styleSpinner.setSelection(face.hairStyle);
-            }
-        });
+		randomButton.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View view) {
+				Log.d("user", "random button click listener");
+				face.randomize();
+				setSeekBarValues(face.getSelectedItemColor());
+				styleSpinner.setSelection(face.hairStyle);
+			}
+		});
 
-        styleSpinner = (Spinner) findViewById(R.id.spinner_hairSelector);
-        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        styleSpinner.setAdapter(adapter);
+		styleSpinner = findViewById(R.id.spinner_hairSelector);
+		final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
+		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		styleSpinner.setAdapter(adapter);
 
-        styleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-                Log.d("user" , "spinner selected item: " + adapterView.getSelectedItem());
-                face.setHairStyle(adapterView.getSelectedItem().toString());
+		styleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+			@Override
+			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+				Log.d("user", "spinner selected item: " + adapterView.getSelectedItem());
+				face.setHairStyle(adapterView.getSelectedItem().toString());
 
-            }
+			}
 
-            @Override
-            public void onNothingSelected(AdapterView<?> adapterView) {
+			@Override
+			public void onNothingSelected(AdapterView<?> adapterView) {
 
-            }
-        });
+			}
+		});
 
-    }
-
-	public int getSelectedItem() {
-		return selectedItem;
 	}
 
-    public int getSelectedItemColor(int itemId) {
-	    int color = -1;
+	protected void setSeekBarValues(int color) {
+		int r = red(color);
+		int g = green(color);
+		int b = blue(color);
 
-	    if(itemId == radioButtonHair.getId()) {
-		    color = face.hairColor;
-
-	    } else if (itemId == radioButtonEyes.getId()) {
-		    color = face.eyeColor;
-	    } else if (itemId == radioButtonSkin.getId()) {
-		    color = face.skinColor;
-	    }
-
-
-	    return color;
-    }
-
-    protected void setSeekBarValues(int color) {
-	    int r = red(color);
-	    int g = green(color);
-	    int b = blue(color);
-
-	    seekBar_r.setProgress(r);
-	    seekBar_g.setProgress(g);
-	    seekBar_b.setProgress(b);
-    }
+		seekBar_r.setProgress(r);
+		seekBar_g.setProgress(g);
+		seekBar_b.setProgress(b);
+	}
 }
