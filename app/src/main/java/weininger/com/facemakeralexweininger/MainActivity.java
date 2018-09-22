2d2cdfb11298305ef1a0569e1fd25c3bc54445aa
Alex <alex.weininger@live.com>
2018-09-21 22:11:47 -0700

    finished layout and strings

--- app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
@@ -3,6 +3,10 @@ package weininger.com.facemakeralexweininger;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
 
 public class MainActivity extends Activity {
 
@@ -10,5 +14,23 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final Spinner styleSpinner = (Spinner) findViewById(R.id.spinner_hairSelector);
+        final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        styleSpinner.setAdapter(adapter);
+
+        styleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> adapterView) {
+
+            }
+        });
+
     }
 }
