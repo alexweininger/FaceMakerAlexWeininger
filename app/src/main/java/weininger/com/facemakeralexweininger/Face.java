d1c20f19645c7a8813badf8457113051ad5ca1a5
Alex <alex.weininger@live.com>
2018-09-22 14:32:12 -0700

    implementing listeners and handlers

--- app/src/main/java/weininger/com/facemakeralexweininger/Face.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/Face.java
@@ -1,9 +1,14 @@
 package weininger.com.facemakeralexweininger;
 
 import android.content.Context;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.SurfaceView;
+
+import java.util.Random;
 
 public class Face extends SurfaceView {
 
@@ -14,19 +19,51 @@ public class Face extends SurfaceView {
 
     public Face(Context context) {
         super(context);
+        setWillNotDraw(false);
         randomize();
     }
 
     public Face(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setWillNotDraw(false);
         randomize();
     }
 
     protected void randomize() {
+        Random random = new Random();
 
+        this.hairStyle = random.nextInt(3);
+
+        this.skinColor = Color.argb(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        this.eyeColor = Color.argb(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+        this.hairColor = Color.argb(random.nextInt(255), random.nextInt(255), random.nextInt(255), random.nextInt(255));
+
+
+
+
+        Log.d("user", "face randomized");
+        invalidate();
     }
 
-    protected void OnDraw() {
+
+    public void onDraw(Canvas canvas) {
+
+
+
+        Log.d("user", "OnDraw");
+        Paint paint = new Paint();
+        paint.setStyle(Paint.Style.FILL);
+        paint.setColor(this.eyeColor);
+        canvas.drawCircle(100, 100, 100, paint);
+
+        paint.setColor(this.skinColor);
+        canvas.drawCircle(300, 100, 100, paint);
+
+        paint.setColor(this.hairColor);
+        canvas.drawCircle(500, 100, 100, paint);
+    }
+
+    protected void drawHairStyle(int hairStyle) {
 
     }
 
@@ -34,6 +71,7 @@ public class Face extends SurfaceView {
         switch (hairStyleString) {
             case "Style 1":
                 this.hairStyle = 1;
+
                 break;
             case "Style 2":
                 this.hairStyle = 2;
@@ -44,9 +82,9 @@ public class Face extends SurfaceView {
             default:
                 this.hairStyle = 0;
                 Log.d("user", "did not find hairStyleString in switch statement - Face.setHairStyle: STRING: " + hairStyleString);
-
-
         }
+        Log.d("user", "style set to: " + this.hairStyle);
+        invalidate();
     }
 
 }
--- app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
@@ -1,22 +1,88 @@
 package weininger.com.facemakeralexweininger;
 
 import android.app.Activity;
+import android.graphics.Canvas;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 import android.widget.Spinner;
+
+import static android.graphics.Color.blue;
+import static android.graphics.Color.green;
+import static android.graphics.Color.red;
 
 public class MainActivity extends Activity {
 
-    @Override
+    Face face;
+    Canvas canvas;
+
+	SeekBar seekBar_r, seekBar_g, seekBar_b;
+
+	@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        final Face face = (Face) findViewById(R.id.face);
+        canvas = new Canvas();
+        face = (Face) findViewById(R.id.face);
+
+        seekBar_r = (SeekBar) findViewById(R.id.seekBar_r);
+        seekBar_g = (SeekBar) findViewById(R.id.seekBar_g);
+        seekBar_b = (SeekBar) findViewById(R.id.seekBar_b);
+
+        Button randomButton = (Button) findViewById(R.id.button_random);
+
+        SeekbarHandler seekbarHandler = new SeekbarHandler(face);
+
+        final RadioGroup radioButtonGroup = (RadioGroup) findViewById(R.id.radioGroup_itemSelector);
+        final RadioButton radioButtonHair = (RadioButton) findViewById(R.id.radioButton_hair);
+        final RadioButton radioButtonEyes = (RadioButton) findViewById(R.id.radioButton_eyes);
+        final RadioButton radioButtonSkin = (RadioButton) findViewById(R.id.radioButton_skin);
+
+        radioButtonGroup.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+                Log.d("user", "radio group onCheckChange listener value: " + i);
+
+                int r, g, b;
+                int color = -1;
+
+	            if(i == radioButtonHair.getId()) {
+					color = face.hairColor;
+
+	            } else if (i == radioButtonEyes.getId()) {
+					color = face.eyeColor;
+	            } else if (i == radioButtonSkin.getId()) {
+					color = face.skinColor;
+	            }
+
+				if(color != -1) {
+					r = red(color);
+					g = green(color);
+					b = blue(color);
+
+					seekBar_r.setProgress(r);
+					seekBar_g.setProgress(g);
+					seekBar_b.setProgress(b);
+				}
+
+            }
+        });
+
+        randomButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d("user", "random button click listener");
+                face.randomize();
+            }
+        });
 
         final Spinner styleSpinner = (Spinner) findViewById(R.id.spinner_hairSelector);
         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
@@ -28,7 +94,6 @@ public class MainActivity extends Activity {
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
                 Log.d("user" , "spinner selected item: " + adapterView.getSelectedItem());
                 face.setHairStyle(adapterView.getSelectedItem().toString());
-
             }
 
             @Override
