bc814a8ac04692a93c9ef3eb2e33956f41cba1e8
Alex <alex.weininger@live.com>
2018-09-22 11:40:14 -0700

    Face class changes

--- app/src/main/java/weininger/com/facemakeralexweininger/Face.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/Face.java
@@ -2,6 +2,7 @@ package weininger.com.facemakeralexweininger;
 
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.SurfaceView;
 
 public class Face extends SurfaceView {
@@ -28,4 +29,24 @@ public class Face extends SurfaceView {
     protected void OnDraw() {
 
     }
+
+    protected void setHairStyle(String hairStyleString) {
+        switch (hairStyleString) {
+            case "Style 1":
+                this.hairStyle = 1;
+                break;
+            case "Style 2":
+                this.hairStyle = 2;
+                break;
+            case "Style 3":
+                this.hairStyle = 3;
+                break;
+            default:
+                this.hairStyle = 0;
+                Log.d("user", "did not find hairStyleString in switch statement - Face.setHairStyle: STRING: " + hairStyleString);
+
+
+        }
+    }
+
 }
--- app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
@@ -3,6 +3,7 @@ package weininger.com.facemakeralexweininger;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -15,6 +16,8 @@ public class MainActivity extends Activity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        final Face face = (Face) findViewById(R.id.face);
+
         final Spinner styleSpinner = (Spinner) findViewById(R.id.spinner_hairSelector);
         final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
@@ -23,6 +26,8 @@ public class MainActivity extends Activity {
         styleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
+                Log.d("user" , "spinner selected item: " + adapterView.getSelectedItem());
+                face.setHairStyle(adapterView.getSelectedItem().toString());
 
             }
 
