7f3c7c1a18de42dd23b00ca0f77725d283a0e429
Alex <alex.weininger@live.com>
2018-09-22 17:35:04 -0700

    done

--- app/src/main/java/weininger/com/facemakeralexweininger/FaceView.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/FaceView.java
@@ -14,7 +14,7 @@ import android.util.AttributeSet;
 import android.view.SurfaceView;
 import java.util.Random;
 
-public class Face extends SurfaceView {
+public class FaceView extends SurfaceView {
 
 	// colors for different parts of face
 	private int skinColor;
@@ -25,14 +25,14 @@ public class Face extends SurfaceView {
 
 	private int selectedItem; // value for the currently selected item (hair, eyes, skin)
 
-	public Face(Context context) {
+	public FaceView(Context context) {
 		super(context);
 		setWillNotDraw(false);
 		randomize();
 		this.selectedItem = 0;
 	}
 
-	public Face(Context context, AttributeSet attrs) {
+	public FaceView(Context context, AttributeSet attrs) {
 		super(context, attrs);
 		setWillNotDraw(false);
 		randomize();
@@ -42,7 +42,7 @@ public class Face extends SurfaceView {
 	protected void randomize() {
 		Random random = new Random();
 
-		this.hairStyle = random.nextInt(3);
+		this.hairStyle = random.nextInt(2) + 1;
 
 		this.skinColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
 		this.eyeColor = Color.argb(255, random.nextInt(255), random.nextInt(255), random.nextInt(255));
@@ -60,38 +60,43 @@ public class Face extends SurfaceView {
 	}
 
 	protected void drawHairStyle(Canvas canvas) {
-		int x = canvas.getWidth();
+
+		int x = canvas.getWidth(); // getting the width and height of canvas
 		int y = canvas.getHeight();
 		int width = y / 4;
 
-		Paint paint = new Paint();
-		paint.setStyle(Paint.Style.FILL);
-		paint.setColor(this.hairColor);
+		Paint hairPaint = new Paint();
+		hairPaint.setStyle(Paint.Style.FILL);
+		hairPaint.setColor(this.hairColor);
 
 		if (this.hairStyle == 1) {
-			canvas.drawCircle(x / 2, y / 2 - width / 3, y / 4, paint);
+			canvas.drawCircle(x / 2, y / 2 - width / 3, y / 4, hairPaint);
 		} else if (this.hairStyle == 2) {
-			canvas.drawRect(x / 2 - 100, y / 2 - width - 40, x / 2 - 80, y / 2 + y / 4, paint);
-			canvas.drawRect(x / 2, y / 2 - width - 50, x / 2 + 20, y / 2 + y / 4, paint);
-			canvas.drawRect(x / 2 + 100, y / 2 - width - 40, x / 2 + 120, y / 2 + y / 4, paint);
-			canvas.drawRect(x / 2 - 140, y / 2 - width - 40, x / 2 - 120, y / 2 + y / 4, paint);
-			canvas.drawRect(x / 2 + 140, y / 2 - width - 40, x / 2 + 160, y / 2 + y / 4, paint);
 
-			canvas.drawRect(x / 2 - width, y / 2 - width, x / 2 + width, y / 2 + y / 4, paint);
+			// drawing rectangles for cool hair
+			canvas.drawRect(x / 2 - 100, y / 2 - width - 40, x / 2 - 80, y / 2 + y / 4, hairPaint);
+			canvas.drawRect(x / 2, y / 2 - width - 50, x / 2 + 20, y / 2 + y / 4, hairPaint);
+			canvas.drawRect(x / 2 + 100, y / 2 - width - 40, x / 2 + 120, y / 2 + y / 4, hairPaint);
+			canvas.drawRect(x / 2 - 140, y / 2 - width - 40, x / 2 - 120, y / 2 + y / 4, hairPaint);
+			canvas.drawRect(x / 2 + 140, y / 2 - width - 40, x / 2 + 160, y / 2 + y / 4, hairPaint);
+
+			canvas.drawRect(x / 2 - width, y / 2 - width, x / 2 + width, y / 2 + y / 4, hairPaint);
+
 		} else if (this.hairStyle == 3) {
-			canvas.drawCircle(x / 2 + 200, y / 2 - width / 3, y / 6, paint);
-			canvas.drawCircle(x / 2, y / 2 - width / 3, y / 6, paint);
-			canvas.drawCircle(x / 2 - 200, y / 2 - width / 3, y / 6, paint);
+
+			canvas.drawCircle(x / 2 + 200, y / 2 - width / 3, y / 6, hairPaint);
+			canvas.drawCircle(x / 2, y / 2 - width / 3, y / 6, hairPaint);
+			canvas.drawCircle(x / 2 - 200, y / 2 - width / 3, y / 6, hairPaint);
 		}
 	}
 
 	// method to draw the skin of the face
 	protected void drawSkin(Canvas canvas) {
-		int x = canvas.getWidth();
+		int x = canvas.getWidth(); // getting the width and height of canvas
 		int y = canvas.getHeight();
 		int radius = y / 3;
 
-		Paint skinPaint = new Paint();
+		Paint skinPaint = new Paint(); // making Paint for the skin
 		skinPaint.setColor(this.skinColor);
 		skinPaint.setStyle(Paint.Style.FILL);
 
@@ -100,11 +105,11 @@ public class Face extends SurfaceView {
 
 	// method to draw two eyes on the face
 	protected void drawEyes(Canvas canvas) {
-		int x = canvas.getWidth();
+		int x = canvas.getWidth(); // getting the width and height of canvas
 		int y = canvas.getHeight();
 		int radius = 70;
 
-		Paint eyePaint = new Paint();
+		Paint eyePaint = new Paint(); // making Paint for the eyes
 		eyePaint.setColor(this.eyeColor);
 		eyePaint.setStyle(Paint.Style.FILL);
 
@@ -115,27 +120,26 @@ public class Face extends SurfaceView {
 	// method that takes a String hairStyle and sets it to the appropriate hairStyle integer
 	protected void setHairStyle(String hairStyleString) {
 		switch (hairStyleString) {
-			case "Style 1":
+			case "Afro Hair":
 				this.hairStyle = 1;
-
 				break;
-			case "Style 2":
+			case "Blocky Hair":
 				this.hairStyle = 2;
 				break;
-			case "Style 3":
+			case "Triple Circle Hair":
 				this.hairStyle = 3;
 				break;
 			default:
-				this.hairStyle = 0;
+				this.hairStyle = 1;
 		}
 		invalidate();
 	}
 
-	public void setSelectedItem(int item) {
+	public void setSelectedItem(int item) { // returns currently selected item
 		this.selectedItem = item;
 	}
 
-	public int getSelectedItemColor() {
+	public int getSelectedItemColor() { // returns the currently selected color
 		if (this.selectedItem == 0) {
 			return this.hairColor;
 		} else if (this.selectedItem == 1) {
@@ -147,6 +151,7 @@ public class Face extends SurfaceView {
 		}
 	}
 
+	// sets the item color to the currently selected item color
 	public void setItemColor(int color) {
 		if (this.selectedItem == 0) {
 			this.hairColor = color;
@@ -155,9 +160,10 @@ public class Face extends SurfaceView {
 		} else if (this.selectedItem == 2) {
 			this.skinColor = color;
 		}
+		invalidate();
 	}
 
-	protected int getHairStyle() {
+	protected int getHairStyle() { // getter for hairStyle
 		return this.hairStyle;
 	}
 }
--- app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
+++ app/src/main/java/weininger/com/facemakeralexweininger/MainActivity.java
@@ -8,7 +8,6 @@
 package weininger.com.facemakeralexweininger;
 
 import android.app.Activity;
-import android.graphics.Canvas;
 import android.graphics.Color;
 import android.os.Bundle;
 import android.view.View;
@@ -26,8 +25,7 @@ import static android.graphics.Color.red;
 
 public class MainActivity extends Activity {
 
-	private Face face;
-	private Canvas canvas;
+	private FaceView faceView;
 	private Spinner styleSpinner; // Spinner for selecting hair style
 	private SeekBar seekBar_r, seekBar_g, seekBar_b; // SeekBars for the r, g, and b components
 
@@ -36,19 +34,18 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		canvas = new Canvas();
-
 		// set the face to the face view
-		face = findViewById(R.id.face);
+		faceView = findViewById(R.id.face);
 
 		Button randomButton = findViewById(R.id.button_random); // random button
 
 		randomButton.setOnClickListener(new View.OnClickListener() { // random button listener
 			@Override
 			public void onClick(View view) {
-				face.randomize();
-				setSeekBarValues(face.getSelectedItemColor()); // update SeekBars
-				styleSpinner.setSelection(face.getHairStyle()); // update Spinner
+				faceView.randomize();
+				setSeekBarValues(faceView.getSelectedItemColor()); // update SeekBars
+				styleSpinner.setSelection(faceView.getHairStyle(), true); // update Spinner
+				faceView.invalidate();
 			}
 		});
 
@@ -63,33 +60,37 @@ public class MainActivity extends Activity {
 			@Override
 			public void onCheckedChanged(RadioGroup radioGroup, int i) {
 				if (i == radioButtonHair.getId()) {
-					face.setSelectedItem(0);
+					faceView.setSelectedItem(0);
 				} else if (i == radioButtonEyes.getId()) {
-					face.setSelectedItem(1);
+					faceView.setSelectedItem(1);
 				} else if (i == radioButtonSkin.getId()) {
-					face.setSelectedItem(2);
+					faceView.setSelectedItem(2);
 				}
 
-				setSeekBarValues(face.getSelectedItemColor()); // update SeekBars
+				setSeekBarValues(faceView.getSelectedItemColor()); // update SeekBars
 			}
 		});
 
+		// setting the Spinner
 		styleSpinner = findViewById(R.id.spinner_hairSelector);
+
+		// creating and setting the adapter and filling the spinner with the values from a string array
 		final ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.styles, android.R.layout.simple_spinner_item);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		styleSpinner.setAdapter(adapter);
 
+		// Spinner listener, handles changing the hairStyle of the face
 		styleSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 			@Override
 			public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
-				face.setHairStyle(adapterView.getSelectedItem().toString()); // set hairStyle to selected hairStyle from spinner
+				faceView.setHairStyle(adapterView.getSelectedItem().toString()); // set hairStyle to selected hairStyle from spinner
 			}
 
 			@Override
 			public void onNothingSelected(AdapterView<?> adapterView) {}
 		});
 
-		// setting the SeekBars
+		// defining the SeekBars
 		seekBar_r = findViewById(R.id.seekBar_r);
 		seekBar_g = findViewById(R.id.seekBar_g);
 		seekBar_b = findViewById(R.id.seekBar_b);
@@ -99,17 +100,16 @@ public class MainActivity extends Activity {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
 
-				int color = face.getSelectedItemColor();
+				int color = faceView.getSelectedItemColor();
 				int blue = blue(color);
 				int green = green(color);
 
 				color = Color.argb(255, i, green, blue);
 
 				if (b) {
-					face.setItemColor(color);
-					face.invalidate();
+					faceView.setItemColor(color);
+					faceView.invalidate();
 				}
-
 			}
 
 			@Override
@@ -122,15 +122,15 @@ public class MainActivity extends Activity {
 		seekBar_g.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-				int color = face.getSelectedItemColor();
+				int color = faceView.getSelectedItemColor();
 				int blue = blue(color);
 				int red = red(color);
 
 				color = Color.argb(255, red, i, blue);
 
 				if (b) {
-					face.setItemColor(color);
-					face.invalidate();
+					faceView.setItemColor(color);
+					faceView.invalidate();
 				}
 			}
 
@@ -144,15 +144,15 @@ public class MainActivity extends Activity {
 		seekBar_b.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
 			@Override
 			public void onProgressChanged(SeekBar seekBar, int i, boolean b) {
-				int color = face.getSelectedItemColor();
+				int color = faceView.getSelectedItemColor();
 				int red = red(color);
 				int green = green(color);
 
 				color = Color.argb(255, red, green, i);
 
 				if (b) {
-					face.setItemColor(color);
-					face.invalidate();
+					faceView.setItemColor(color);
+					faceView.invalidate();
 				}
 			}
 
@@ -163,13 +163,11 @@ public class MainActivity extends Activity {
 		});
 	}
 
-	private void setSeekBarValues(int color) {
-		int r = red(color);
-		int g = green(color);
-		int b = blue(color);
+	private void setSeekBarValues(int color) { // sets SeekBar progress(s) to a given color
 
-		seekBar_r.setProgress(r);
-		seekBar_g.setProgress(g);
-		seekBar_b.setProgress(b);
+		// set SeekBar progress to (r, g, b) color component of the color
+		seekBar_r.setProgress(red(color));
+		seekBar_g.setProgress(green(color));
+		seekBar_b.setProgress(blue(color));
 	}
 }
